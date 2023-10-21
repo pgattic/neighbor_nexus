d16e979ac6e0cfd9bf4c75cda0a858eef488b323
BryantVanOrden <bryantv@byui.edu>
2023-10-21 08:06:10 -0600

    fix

--- lib/event.dart
+++ lib/event.dart
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
+
 // import 'dart:async';
 // import 'dart:collection';
 // import 'dart:js_util';
 // import 'package:flutter/material.dart';
 // import 'package:google_maps_flutter/google_maps_flutter.dart';
-=======
-import 'dart:async';
-import 'dart:collection';
-import 'dart:js_util';
-import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:map_view/map_view.dart';
->>>>>>> eab997061f306291832c11b8126d5cc007823996
+
 
 
 // class Event {
@@ -23,13 +16,6 @@ import 'package:map_view/map_view.dart';
 //   Event (this.name, this.location, this.time, this.explanation);
 // }
 
-<<<<<<< HEAD
-// class EventManager {
-//   List<Event> events = [];
-=======
-  StreamController<Event>
-  eventController = StreamController.broadcast();
->>>>>>> eab997061f306291832c11b8126d5cc007823996
 
 //   StreamController<Event>
 //   eventController = new
@@ -39,7 +25,7 @@ import 'package:map_view/map_view.dart';
 //   {
 //     events.add (event);
 
-<<<<<<< HEAD
+
 //     eventController.add(event);
 //   }
 
@@ -47,22 +33,7 @@ import 'package:map_view/map_view.dart';
 //   eventStream =>
 //   eventController.stream;
 // }
-=======
-  Stream<Event> get
-  eventStream =>
-  eventController.stream;
 
-  void dispose() {
-    eventController.close();
-  }
-}
-
-class MapViewWidget extends
-StatefulWidget{
-  @override
-  _MapViewWidgetState createState() =>_MapViewWidgetState();
-}
->>>>>>> eab997061f306291832c11b8126d5cc007823996
 
 // class MapViewWidget extends
 // StatefulWidget{
@@ -78,27 +49,16 @@ StatefulWidget{
 //   }
 // }
 
-<<<<<<< HEAD
 // class _MapViewWidgetState extends State<MapViewWidget>
 // {
 //   MapView mapView = new MapView();
-=======
-  @override
-  void dispose() {
-  eventManager.dispose();
-  super.dispose();
-  }
-  @override
-  void initState() {
-    super.initState();
->>>>>>> eab997061f306291832c11b8126d5cc007823996
+
 
 //   EventManager eventManager = new EventManager();
 
 //   @override void initState() {
 //     super.initState();
 
-<<<<<<< HEAD
 //     mapView.show(MapOptions(
 //       showUserLocation:true,
 
@@ -132,32 +92,7 @@ StatefulWidget{
 //             "Sample Event", new Location(43.491651, -112.033964) as String, DateTime.now(), "This is a sample event",
 //           );
 //           eventManager.addEvent(sampleEvent);
-=======
-    eventManager.eventStream.listen((event) {
-      mapView.addMarker(
-        new Marker (
-          event.name,
-          event.explanation,
-          event.location.latitude,
-          event.location.longitude,
-          color:Colors.red,
-        ),
-      );
-    });
-  }
-  @override
-  Widget build (BuildContext context) {
-    return Scaffold(
-      appBar: AppBar (title: Text ("Map View Widget"),),
-      body: Center(child: Text ("This is a placeholder for the map view"),),
-      floatingActionButton: FloatingActionButton (
-        onPressed: () {
-          //a sample event
-          Event sampleEvent = new Event(
-            "Sample Event", new Location(43.491651, -112.033964), DateTime.now(), "This is a sample event",
-          );
-          eventManager.addEvent(sampleEvent);
->>>>>>> eab997061f306291832c11b8126d5cc007823996
+
       
 //         },
 //         child: Icon(Icons.add),
