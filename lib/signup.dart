761273acf1b1ab89b11eb64e612bfe385ab15e8b
BryantVanOrden <bryantv@byui.edu>
2023-10-21 10:32:32 -0600

    hello

--- lib/signup.dart
+++ lib/signup.dart
@@ -3,16 +3,14 @@ import 'package:neighbor_nexus/firebase/auth_provider.dart';
 import 'package:neighbor_nexus/login.dart';
 import 'package:neighbor_nexus/main.dart';
 import 'package:provider/provider.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class SignUpPage extends StatelessWidget {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController displayNameController = TextEditingController();
-  // Remove the iconController
 
   SignUpPage({super.key});
-  
- 
 
   @override
   Widget build(BuildContext context) {
@@ -24,19 +22,17 @@ class SignUpPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             // Your logo here
-            // For example, an Image widget
             Image.asset(
               'assets/images/logo.png',
-              width: 150, // Adjust the size as needed
-              height: 150, // Adjust the size as needed
+              width: 150,
+              height: 150,
             ),
-            const SizedBox(height: 16), // Add spacing
+            const SizedBox(height: 16),
             const Text(
               'Sign Up',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height: 16), // Add spacing
-            // Curved text fields
+            const SizedBox(height: 16),
             TextFormField(
               controller: emailController,
               decoration: InputDecoration(
@@ -46,7 +42,7 @@ class SignUpPage extends StatelessWidget {
                 ),
               ),
             ),
-            const SizedBox(height: 8), // Add more spacing
+            const SizedBox(height: 8),
             TextFormField(
               controller: passwordController,
               decoration: InputDecoration(
@@ -56,7 +52,7 @@ class SignUpPage extends StatelessWidget {
                 ),
               ),
             ),
-            const SizedBox(height: 8), // Add more spacing
+            const SizedBox(height: 8),
             TextFormField(
               controller: displayNameController,
               decoration: InputDecoration(
@@ -66,23 +62,40 @@ class SignUpPage extends StatelessWidget {
                 ),
               ),
             ),
-            // Remove the TextField for the icon
-            const SizedBox(height: 16), // Add more spacing
+            const SizedBox(height: 16),
             ElevatedButton(
               onPressed: () {
-                Provider.of<AuthProvider>(context, listen: false).signUp(
-                  email: emailController.text,
-                  password: passwordController.text,
-                  displayName: displayNameController.text,
-                );
-                Navigator.of(context).push(MaterialPageRoute(builder: (context) => HomeScreen()));
+                final password = passwordController.text;
+                if (password.length < 6) {
+                  // Show a toast message for password requirements
+                  Fluttertoast.showToast(
+                    msg: 'Password must be at least 6 characters',
+                    toastLength: Toast.LENGTH_SHORT,
+                  );
+                } else {
+                  Provider.of<AuthProvider>(context, listen: false)
+                      .signUp(
+                    email: emailController.text,
+                    password: password,
+                    displayName: displayNameController.text,
+                  )
+                      .then((result) {
+                    // Successfully signed up
+                    Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (context) => LoginPage()));
+                  }).catchError((error) {
+                    // Handle the error here
+                    print('Error signing up: $error');
+                  });
+                }
               },
               child: const Text('Sign Up'),
             ),
             TextButton(
               onPressed: () {
                 // Navigate to the login page
-                Navigator.of(context).push(MaterialPageRoute(builder: (context) => LoginPage()));
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => LoginPage()));
               },
               child: const Text("Already have an account? Login"),
             ),
