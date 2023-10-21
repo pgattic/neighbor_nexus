72b85a7c0d8286472f654108a602cc9a3ebd1277
themount <themount707@gmail.com>
2023-10-21 15:39:46 -0600

    this is new ui

--- lib/event_view_screen.dart
+++ lib/event_view_screen.dart
@@ -5,15 +5,12 @@ import 'package:neighbor_nexus/firebase/auth_provider.dart';
 import 'package:provider/provider.dart';
 
 class EventDetailPage extends StatelessWidget {
-  final Event event; // Event object
-  
-
-
+  final Event event;
 
   EventDetailPage({
     required this.event,
   });
-  
+
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<AuthProvider>(context).user;
@@ -22,52 +19,89 @@ class EventDetailPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Event Details'),
+        backgroundColor: Colors.blue,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          ListTile(
-            title: Text(event.title),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Date and Time: ${event.dateTime.toString()}'),
-                Text('User ID: ${event.userId}'),
-              ],
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.event, color: Colors.blue, size: 40.0),
+              title: Text(
+                event.title,
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(height: 10),
+                  Text('Date and Time: ${event.dateTime.toString()}'),
+                  SizedBox(height: 10),
+                  Text('User ID: ${event.userId}'),
+                ],
+              ),
             ),
-          ),
-          // Add any other event details you want to display here
+            SizedBox(height: 20),
+            if (!isCurrentUserEventCreator)
+              Expanded(
+                child: ChatScreen(recipientUid: event.userId,),
+              )
+            else
+              Container(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                  ),
+                  onPressed: () => _confirmDelete(context, event),
+                  child: Text('Delete Event'),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
 
-          if (!isCurrentUserEventCreator)
-            Expanded(
-              child: ChatScreen(recipientUid: event.userId,), // Show the chat screen if the user is not the event creator
-            )
-          else
-            ElevatedButton(
-              onPressed: () {
-                // Delete event logic here
-                onDeleteEvent(event,context);
-              },
-              child: Text('Delete Event'),
-            ),
+  void _confirmDelete(BuildContext context, Event event) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text("Delete Event"),
+        content: Text("Are you sure you want to delete this event?"),
+        actions: [
+          TextButton(
+            child: Text("Cancel"),
+            onPressed: () => Navigator.of(ctx).pop(),
+          ),
+          TextButton(
+            child: Text("Delete"),
+            onPressed: () {
+              onDeleteEvent(event, context);
+              Navigator.of(ctx).pop();
+            },
+          ),
         ],
       ),
     );
   }
-}
 
-void onDeleteEvent(event,context) {
-  // Assuming you have a reference to your Firestore collection
-  final CollectionReference eventsCollection = FirebaseFirestore.instance.collection('events');
+  void onDeleteEvent(Event event, BuildContext context) {
+    final CollectionReference eventsCollection = FirebaseFirestore.instance.collection('events');
+    final eventDocRef = eventsCollection.doc(event.eventId);
 
-  // Replace 'event.eventId' with the actual identifier you use for your events
-  final eventDocRef = eventsCollection.doc(event.eventId);
-
-  eventDocRef.delete().then((_) {
-    // Event successfully deleted, you can navigate back to the previous screen or perform other actions
-    Navigator.pop(context);
-  }).catchError((error) {
-    // Handle any errors that occur during the delete process
-    print('Error deleting event: $error');
-  });
+    eventDocRef.delete().then((_) {
+      Navigator.pop(context);
+    }).catchError((error) {
+      print('Error deleting event: $error');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Error deleting event. Please try again.'),
+          backgroundColor: Colors.red,
+        ),
+      );
+    });
+  }
 }
